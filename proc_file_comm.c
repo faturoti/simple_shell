1974466e52aa2efb7018488baa981ceeb3e786d2
faturoti <abfat049@gmail.com>
2023-12-13 10:59:55 -0800

    To process file for command

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -1,11 +1,15 @@
 #include "main.h"
+
 int cant_open(char *file_path);
+int proc_file_commands(char *file_path, int *exe_ret);
+
 /**
  * cant_open - If the file doesn't exist or lacks proper permissions
  * @file_path: Path to the supposed file.
  *
  * Return - 127
  */
+
 int cant_open(char *file_path)
 {
 	char *error, *hist_str;		//Create memory loc for both
@@ -20,6 +24,7 @@ int cant_open(char *file_path)
 
 	len = _str_len(name) + _strlen(hist_str) + _strlen(file_path) + 16;
 	error = malloc(sizeof(char) * (len + 1));
+	
 	if (!error)
 	{
 		free(hist_str);
@@ -72,7 +77,7 @@ int proc_file_commands(char *file_path, int *exe_ret)
 	//Line automatically has size of 120
 	line = malloc(sizeof(char) * old_size);
 
-	if(!line)
+	if (!line)
 	{
 		return (-1);
 	}
@@ -84,6 +89,67 @@ int proc_file_commands(char *file_path, int *exe_ret)
 			return (*exe_ret);
 
 		buffer[b_read] = '\0';	//Null terminate the buffer
-		
+		line_size += b_read;
+		line = _realloc(line, old_size, line_size);
+		_strcat(line, buffer);
+
+		old_size = line_size;
+
 	}
+	while (b_read);
+
+	for (i = 0; line[i] == '\n'; i++)
+		line[i] = ' ';
+
+	for (; i < line_size; i++)
+	{
+		if (line[i] == '\n')
+		{
+			line[i] = ';';
+			for (i += 1; i < line_size && line[i] == '\n'; i++)
+			{
+				line[i] = ' ';
+			}
+		}
+
+	}
+
+	variable_replacement(&line, exe_ret);
+	handle_line(&line, line_size);
+	handle_line(&line, line_size);
+
+	args = _strtok(line, " ");
+	free(line);
+
+	if (!args)
+	{
+		return (0);
+	}
+
+	if (check_args(args) != 0)
+	{
+		*exe_ret = 2;
+		free_args(args, args);
+		return (*exe_ret);
+	}
+
+	front = args;
+
+	for (i = 0; args[i]; i++)
+	{
+		if (_strncmp(args[i], ";", 1) == 0)
+		{
+			free(args[i]);
+			args[i] = NULL;
+			ret = call_args(args, front, exe_ret);
+			args = &args[++i];
+			i = 0;
+		}
+	}
+
+	ret = call_args(args, front, exe_ret);
+	
+	free(front);
+
+	return (ret);
 }
