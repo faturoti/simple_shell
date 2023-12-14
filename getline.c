857825dad3c9dd5005070e4927f3c09bb0db62b5
faturoti <abfat049@gmail.com>
2023-12-14 12:59:10 -0800

    Some classes

--- getline.c
+++ getline.c
@@ -13,22 +13,28 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;	//Pointer to new memory location
-	char *filler, *ptr_copy;	//Duplicate pointer for mem and ptr
+	void *mem;	
+	/*Pointer to new memory location*/
+	char *filler;
+	char *ptr_copy;	
+	/*Duplicate pointer for mem and ptr*/
 	unsigned int index;
 
-	if (new_size == old_size)	//Nothing to do
+	if (new_size == old_size)	
+		/*Nothing to do*/
 	{
 		return(ptr);
 	}
 
-	if (new_size == 0 && ptr != NULL)	//Just free PTR and leave
+	if (new_size == 0 && ptr != NULL)	
+		/*Just free PTR and leave*/
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (ptr == NULL)	//Just allocate the new size specified by new_size
+	if (ptr == NULL)	
+	/*Just allocate the new size specified by new_size*/
 	{
 		mem = malloc(new_size);
 		if (mem == NULL)
@@ -39,17 +45,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (mem);
 	}
 
-	//Otherwise do this below
+	/*Otherwise do this below*/
 	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);	//allocate space by factor of new_size
+	mem = malloc(sizeof(*ptr_copy) * new_size);	
+	/*allocate space by factor of new_size*/
 
-	if (mem == NULL)	//If the allocation fails, return NULL
+	if (mem == NULL)	
+	/*If the allocation fails, return NULL*/
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	filler = mem;	//create duplicate for mem to fill memory
+	filler = mem;	
+	/*create duplicate for mem to fill memory*/
 
 	for (index = 0; index < old_size && index < new_size; index++)
 	{
@@ -59,4 +68,45 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	free(ptr);
 
 	return (mem);
+}
+
+/**
+ * assign_lineptr - Reassigns the lineptr variable for _getline.
+ * @lineptr: A buffer to store an input string.
+ * @n: The size of lineptr.
+ * @buffer: The string to assign to lineptr.
+ * @b: The size of buffer.
+ */
+void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
+{
+	if (*lineptr == NULL)
+	{
+		if (b > 120)
+		{
+			*n = b;
+		}
+		else
+		{
+			*n = 120;
+		}
+		*lineptr = buffer;
+	}
+	else if (*n > b)
+	{
+		if (b > 120)
+		{
+			*n = b;
+		}
+		else
+		{
+			*n = 120;
+		}
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+
 }
