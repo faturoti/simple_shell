e5fe27e65eed43401036d4ab2f1e2bcaa6dc505b
faturoti <abfat049@gmail.com>
2023-12-15 13:37:50 -0800

    This is for the helpers

--- getline.c
+++ getline.c
@@ -110,3 +110,71 @@ void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 	}
 
 }
+
+/**
+ * _getline - Reads input from a stream.
+ * @lineptr: A buffer to store the input.
+ * @n: The size of lineptr.
+ * @stream: The stream to read from.
+ *
+ * Return: The number of bytes read.
+ */
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	static ssize_t input;
+	ssize_t ret;
+	char c = 'x';
+       	char *buffer;
+	int r;
+
+	if (input == 0)
+	{
+		fflush(stream);
+	}
+	else
+	{
+		return (-1);
+	}
+
+	input = 0;
+
+	buffer = malloc(sizeof(char) * 120);
+	if (!buffer)
+	{
+		return (-1);
+	}
+
+	while (c != "\n")
+	{
+		r = read(STDIN_FILENO, &c, 1);
+		if (r == -1 || (r == 0 && input == 0))
+		{
+			free (buffer);
+			retun (-1);
+		}
+		if (r == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= 120)
+		{
+			buffer = _realloc(buffer, input, input + 1);
+		}
+		buffer[input] = c;
+		input++;
+	}
+	buffer[input] = '\0';
+
+	assign_lineptr(lineptr, n, buffer, input);
+	ret = input;
+
+	if (r != 0)
+	{
+		input = 0;
+	}
+
+	return (ret);	
+
+}
